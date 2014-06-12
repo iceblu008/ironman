8bc3ede3c27aef0039707d78fdd0fdc4565d5f03
iceblu008 <domenic.cordova@yahoo.com>
2014-06-11 19:35:03 -0700

    Revert "initial commit"
    This reverts commit e1ecf533e14f4ed23cc2c5d68ad54b16905bae60.

--- main/java/com/ironman/lib/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.ironman.lib;
-
-public class Strings {
-
-	public static final String MODID = "ironman";
-	public static final  String name = "The Iron Man mod";
-	public static final String version = "1.0";
-	
-}
